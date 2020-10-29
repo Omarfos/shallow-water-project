ede65d921f749ff761c085d4c7610ab9ef774e06
xiamengqi2012 <mandyxmq@gmail.com>
2020-10-29 17:21:32 -0400

    tuning scripts

--- run.sh
+++ run.sh
@@ -4,13 +4,13 @@ strongdir=script/strong
 weakdir=script/weak
 
 # strong scaling analysis (--exclusive flag avoid jobs colliding on nodes)
-sbatch --exclusive $strongdir/shallow1.sub
-sbatch --exclusive $strongdir/shallow2.sub
-sbatch --exclusive $strongdir/shallow3.sub
-sbatch --exclusive $strongdir/shallow4.sub
-sbatch --exclusive $strongdir/shallow5.sub
-sbatch --exclusive $strongdir/shallow6.sub
-sbatch --exclusive $strongdir/shallow7.sub
+# sbatch --exclusive $strongdir/shallow1.sub
+# sbatch --exclusive $strongdir/shallow2.sub
+# sbatch --exclusive $strongdir/shallow3.sub
+# sbatch --exclusive $strongdir/shallow4.sub
+# sbatch --exclusive $strongdir/shallow5.sub
+# sbatch --exclusive $strongdir/shallow6.sub
+# sbatch --exclusive $strongdir/shallow7.sub
 
 # batch run weak scaling analysis
 # sbatch --exclusive $weakdir/shallow1_weak.sub
@@ -20,3 +20,16 @@ sbatch --exclusive $strongdir/shallow7.sub
 # sbatch --exclusive $weakdir/shallow5_weak.sub
 # sbatch --exclusive $weakdir/shallow6_weak.sub
 # sbatch --exclusive $weakdir/shallow7_weak.sub
+
+# batch run block tuning
+blockdir=script/block
+for i in 400 800 1600
+do
+    for j in 32 64 128 256
+    do
+	for k in 2 4 6 8
+	do
+	    sbatch --exclusive $blockdir/grid${i}_block${j}_batch${k}.sub
+	done
+    done
+done
